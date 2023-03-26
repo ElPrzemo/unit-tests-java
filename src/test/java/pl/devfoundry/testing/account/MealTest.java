9876a558169e3d29cb8bd9828eed6168ecd31061
przem <przemyslawwlodarczyk90@gmail.om>
2023-03-26 15:11:37 +0200

    DODAJEMY TESTY DYNAMICZNE

--- src/test/java/pl/devfoundry/testing/account/MealTest.java
+++ src/test/java/pl/devfoundry/testing/account/MealTest.java
@@ -1,18 +1,25 @@
 package pl.devfoundry.testing.account;
 
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Stream;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+import org.junit.jupiter.api.function.Executable;
 
 class MealTest {
 
@@ -126,5 +133,42 @@ class MealTest {
 
         assertThat(price, lessThan(20));
     };
+
+   @TestFactory
+    Collection<DynamicTest> dynamicTestCollection(){
+       return Arrays.asList(
+               dynamicTest("Dynamic test 2", ()->assertThat(6, lessThan(6))),
+               dynamicTest("Dynamic test 2", ()->assertEquals(4, 2*2))
+       );
+   }
+
+   @TestFactory
+   Collection<DynamicTest> calculateMealPrices() {
+       Order order = new Order();
+       order.addMealToOOrder(new Meal(10, 2, "Hamburger"));
+       order.addMealToOOrder(new Meal(7, 4, "Fries"));
+       order.addMealToOOrder(new Meal(22, 3, "Pizza"));
+
+       Collection<DynamicTest> dynamicTests = new ArrayList<>();
+
+       for(int i = 0; i < order.getMeals().size(); i++) {
+
+           int price = order.getMeals().get(i).getPrice();
+           int quantity = order.getMeals().get(i).getQuantity();
+
+           Executable executable = () -> {
+               assertThat(calculatePrice(price, quantity), lessThan(67));
+           };
+
+           String name = "Test name: " + i;
+           DynamicTest dynamicTest = DynamicTest.dynamicTest(name, executable);
+           dynamicTests.add(dynamicTest);
+       }
+
+       return dynamicTests;
+   }
+    private int calculatePrice (int price, int quantity){
+    return price *quantity;
+   }
 
 }
